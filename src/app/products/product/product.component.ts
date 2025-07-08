cc4800583e72b4ea51f9ac9c12272fdf1e3b3e17
Máté Eke <ekemate93@gmail.com>
2025-07-08 22:53:45 +0200

    implement reactive form

--- src/app/products/product/product.component.ts
+++ src/app/products/product/product.component.ts
@@ -8,14 +8,16 @@ import {
   input,
   signal,
 } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 import { ImageWithFallbackComponent } from '../../common/image-with-fallback/image-with-fallback.component';
 import { CartService } from '../../common/services/cart.service';
 import { Product } from '../../common/services/products.service';
 
 @Component({
   selector: 'app-product',
-  imports: [CurrencyPipe, ImageWithFallbackComponent, FormsModule],
+  imports: [CurrencyPipe, ImageWithFallbackComponent, ReactiveFormsModule],
   templateUrl: './product.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -23,8 +25,12 @@ export class ProductComponent {
   private readonly cartService = inject(CartService);
 
   product = input.required<Product>();
+  inputControl = new FormControl(0);
 
-  inputValue = signal(0);
+  inputValue = toSignal(
+    this.inputControl.valueChanges.pipe(debounceTime(500)),
+    { initialValue: 0 },
+  );
   cartAmount = signal(0);
 
   computedStock = computed(() => {
@@ -33,26 +39,37 @@ export class ProductComponent {
 
   constructor() {
     effect(() => {
-      this.cartAmount.set(this.cartService.amountInCart(this.product()._id));
+      if (this.product()) {
+        this.inputControl.clearValidators();
+        this.inputControl.addValidators([
+          Validators.min(this.product().minOrderAmount),
+          Validators.max(this.product().availableAmount),
+        ]);
+        this.inputControl.updateValueAndValidity();
+        this.cartAmount.set(this.cartService.amountInCart(this.product()._id));
+      }
     });
     effect(() => {
       if (this.cartAmount() > 0) {
-        this.inputValue.set(this.cartAmount());
+        this.inputControl.setValue(this.cartAmount());
       }
+    });
+    effect(() => {
+      this.updateCart(this.inputValue() ?? 0);
     });
   }
 
-  updateCart() {
+  updateCart(value: number) {
     if (
-      !this.inputValue() ||
-      this.inputValue() < this.product().minOrderAmount ||
-      this.inputValue() > this.product().availableAmount
+      !value ||
+      value < this.product().minOrderAmount ||
+      value > this.product().availableAmount
     ) {
       this.cartService.deleteFromCart(this.product()._id);
       this.cartAmount.set(0);
     } else {
-      this.cartService.addToCart(this.product()._id, this.inputValue());
-      this.cartAmount.set(this.inputValue());
+      this.cartService.addToCart(this.product()._id, value);
+      this.cartAmount.set(value);
     }
   }
 }
